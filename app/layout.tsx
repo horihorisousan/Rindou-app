f81806721b9828186caedbdd60af4b76255fc4a2
horihorisousan <horisou.k@gmail.com>
2025-11-07 11:14:54 +0900

    AdSense code snippet
    AdSense code snippet

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,16 +31,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ja">
-      <head>
-        {process.env.NEXT_PUBLIC_GOOGLE_ADSENSE_ID && (
-          <Script
-            async
-            src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_GOOGLE_ADSENSE_ID}`}
-            crossOrigin="anonymous"
-            strategy="afterInteractive"
-          />
-        )}
-      </head>
+      <Script 
+        async 
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2247568702185856" 
+        crossOrigin="anonymous"
+        strategy="afterInteractive" 
+      />
       <body className={notoSansJp.className} style={{
         margin: 0,
         padding: 0,
